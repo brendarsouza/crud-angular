df36dbe891c047d32d282a1ffe0f9d1d87ee2d13
Brenda Souza <si.brendarenata@gmail.com>
2023-04-22 23:28:20 -0300

    feat: remove course

--- src/app/courses/containers/courses/courses.component.ts
+++ src/app/courses/containers/courses/courses.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
 import { catchError, Observable, of } from 'rxjs';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 import { Course } from '../../model/course';
 import { CoursesService } from '../../services/courses.service';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -13,21 +14,16 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
-  courses$: Observable<Course[]>;
+  courses$: Observable<Course[]> | null = null;
 
   constructor(
     private coursesServices: CoursesService,
     public dialog: MatDialog,
     private router: Router,
+    private snackBar: MatSnackBar,
     private route: ActivatedRoute
   ) {
-    this.courses$ = this.coursesServices.list()
-    .pipe(
-      catchError((error) => {
-        this.onError('Erro ao carregar cursos.');
-        return of([]);
-      })
-    );
+    this.refresh();
   }
 
   onAdd() {
@@ -38,10 +34,34 @@ export class CoursesComponent {
     this.router.navigate(['edit', course._id], { relativeTo: this.route });
   }
 
+  onRemove(course: Course) {
+    this.coursesServices.remove(course._id).subscribe(
+      () => {
+        this.refresh();
+        this.snackBar.open('Curso removido com sucesso', 'X', {
+          duration: 3000,
+          verticalPosition: 'top',
+          horizontalPosition: 'center',
+        });
+      },
+      () => {
+        this.onError('Erro ao tentar remover curso!');
+      }
+    );
+  }
+
+  refresh() {
+    this.courses$ = this.coursesServices.list().pipe(
+      catchError((error) => {
+        this.onError('Erro ao carregar cursos.');
+        return of([]);
+      })
+    );
+  }
+
   onError(errorMSG: string) {
     this.dialog.open(ErrorDialogComponent, {
       data: errorMSG,
     });
   }
-
 }
